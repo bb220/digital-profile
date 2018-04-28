70fd80c9669ec96d0f9516a31ded4fe8e25719f5
Brandon Bellero <brandon.bellero@jcrew.com>
2018-04-28 19:57:00 -0400

    feat: add svg logo

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,21 +2,24 @@ import React from 'react'
 import css from './style.module.scss'
 import Footer from './footer/component'
 import Link from 'gatsby-link'
+import logo from './logo.svg'
 
 export default ({ children, location }) => {
   const subtitle = (location.pathname).substring(1)
   return (
     <div className={css.app_container}>
       <div className={css.top}>
-        <h1><Link to='/'>B</Link></h1>
+        <Link to='/'>
+          <object className={css.object} type='image/svg+xml' data={logo}></object>
+        </Link>
         <h3 className={css.subtitle}>{subtitle}</h3>
       </div>
       <div className={css.bottom}>
         <div className={css.content_container}>
           {children()}
-          <Footer />
         </div>
       </div>
+      <Footer />
     </div>
   )
 }
