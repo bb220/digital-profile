b5f81a40bc45c05cee29df87dd2979da3e6e7811
Brandon Bellero <brandon.bellero@jcrew.com>
2018-05-01 19:05:41 -0400

    feat: add support for json content

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,13 @@ module.exports = {
   plugins: [
     'gatsby-link',
     'gatsby-plugin-sass',
+    'gatsby-transformer-json',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/src/content/`,
+      },
+    }
   ]
 }
