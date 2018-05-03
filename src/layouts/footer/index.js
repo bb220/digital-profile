41cca9baf53e181f9a6fce2048f26e5a359889c8
Brandon Bellero <brandon.bellero@jcrew.com>
2018-05-02 20:04:40 -0400

    refactor: footer styles

--- src/layouts/footer/index.js
+++ src/layouts/footer/index.js
@@ -7,18 +7,21 @@ import gh from './gh.png'
 export default function Footer ({ data }) {
   return (
     <footer className={css.footer}>
-      <div className={css.phrase}>
-        <p>Let's collaborate.</p>
+      <div className={css.line}></div>
+      <div className={css.contact_container}>
+        <div className={css.phrase}>
+          <p>Let's collaborate.</p>
+        </div>
+        <a href={data.linkedInUrl}>
+          <img src={linkedIn} />
+        </a>
+        <a href={`mailto:${data.email}`}>
+          <img className={css.email} src={email} />
+        </a>
+        <a href={data.github}>
+          <img src={gh} />
+        </a>
       </div>
-      <a href={data.linkedInUrl}>
-        <img src={linkedIn} />
-      </a>
-      <a href={`mailto:${data.email}`}>
-        <img className={css.email} src={email} />
-      </a>
-      <a href={data.github}>
-        <img src={gh} />
-      </a>
     </footer>
   )
 }
