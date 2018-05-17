d1f3ee4e156080149a1ef3b74a5c12dd2917754a
Brandon Bellero <brandon.bellero@jcrew.com>
2018-05-16 21:03:18 -0400

    feat: add simple opacity effect

--- src/components/top-section/index.js
+++ src/components/top-section/index.js
@@ -1,27 +1,56 @@
-import React from 'react'
+import React, { Component } from 'react'
 import css from './style.module.scss'
 import Link from 'gatsby-link'
 import NavBar from '../navbar'
 import cx from 'classnames'
 
-export default ({ subtitle, className, logo }) => {
-  return (
-    <div>
-    <div className={cx({
-        [css.logo]: true,
-        [className]: true
-      })}>
-      <NavBar className={css.link_color} />
-      <Link to='/'>
-        <object className={css.object} type='image/svg+xml' data={logo}></object>
-      </Link>
-    </div>
-    <div className={cx({
-        [css.top]: true,
-        [className]: true
-      })}>
-      <h3 className={css.subtitle}>{subtitle}</h3>
-    </div>
-    </div>
-  )
+export default class TopSection extends Component {
+  constructor() {
+    super()
+    this.state = {
+      scrollHeight: 0
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.onScroll)
+  }
+
+  onScroll = () => {
+    this.setState({ scrollHeight: window.scrollY })
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.onScroll)
+  }
+
+
+  render() {
+    const {
+      subtitle,
+      className,
+      logo
+    } = this.props
+    const opacity = Math.min((30/ this.state.scrollHeight) -.3 , 1)
+    return (
+      <div>
+      <div className={cx({
+          [css.logo]: true,
+          [className]: true
+        })}>
+        <NavBar className={css.link_color} />
+        <Link to='/'>
+          <object className={css.object} type='image/svg+xml' data={logo}></object>
+        </Link>
+      </div>
+      <div
+        className={cx({
+          [css.top]: true,
+          [className]: true
+        })}>
+          <h3 style={'opacity', {opacity}}className={css.subtitle}>{subtitle}</h3>
+      </div>
+      </div>
+    )
+  }
 }
