d43b0ed8b61bd76904cde5109934f44ff42a360c
bb220 <bellero.brandon@gmail.com>
2019-02-23 12:38:58 -0500

    refactor: change dynamic opacity code

--- src/components/top-section/index.js
+++ src/components/top-section/index.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react'
 import css from './style.module.scss'
-import { Link } from 'gatsby'
-import NavBar from '../navbar'
 import cx from 'classnames'
 
 export default class TopSection extends Component {
@@ -29,10 +27,13 @@ export default class TopSection extends Component {
   render() {
     const {
       subtitle,
-      className,
-      logo
+      className
     } = this.props
-    const opacity = Math.min((30/ this.state.scrollHeight) -.3 , 1)
+
+    const dynamicOpacity = {
+      opacity: Math.min((30 / this.state.scrollHeight) - .3, 1)
+    }
+
     return (
         <div
           className={cx({
@@ -40,7 +41,7 @@ export default class TopSection extends Component {
             [className]: true
           })}>
           <div className={css.subtitle_container}>
-            <h3 style={'opacity', {opacity}} className={css.subtitle}>{subtitle}</h3>
+            <h3 style={dynamicOpacity} className={css.subtitle}>{subtitle}</h3>
           </div>
       </div>
     )
